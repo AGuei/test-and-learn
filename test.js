c0f5bbc571b6453f2522f11aa5601c79f41354d1
貴 <AGuei@users.noreply.github.com>
2017-10-14 17:52:12 +0800

    使單選按鈕排在項目符號前面，藉由css偽元素來達成

--- test.js
+++ test.js
@@ -3,21 +3,24 @@ let firstOl = document.createElement('ol');
 for(let i=0; i<5; i++){
     let firstLi = document.createElement('li');
     let firstLiText = document.createTextNode('test'+i);
-    let secondOl = document.createElement('ol');
-    secondOl.type = 'A';
+    let secondOl = document.createElement('ol');    
     firstLi.appendChild(firstLiText);
     for(let j=5; j<9; j++){
-        let secondLi = document.createElement('li');    
+        let secondLi = document.createElement('li');
         let secondLiText = document.createTextNode('test'+j);
         let radioButton = document.createElement('input');
+        let label = document.createElement('label');
+        let br = document.createElement('br');
         radioButton.type = 'radio';
-        radioButton.name = 'radio' + '_' + i;
-        secondLi.appendChild(radioButton);
+        radioButton.name = 'radio' + '_' + i;        
         secondLi.appendChild(secondLiText);
-        secondOl.appendChild(secondLi);        
-        firstLi.appendChild(secondOl);        
+        label.appendChild(radioButton);
+        label.appendChild(secondLi);
+        label.appendChild(br);
+        secondOl.appendChild(label);
+        firstLi.appendChild(secondOl);
     }
-    firstOl.appendChild(firstLi);    
+    firstOl.appendChild(firstLi);
 }
 fragment.appendChild(firstOl);
 document.body.appendChild(fragment);
