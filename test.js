29bb00859f264557ba38ba2604179999f966140f
貴 <AGuei@users.noreply.github.com>
2017-10-14 20:26:11 +0800

    改變文字節點的內容

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ let firstOl = document.createElement('ol');
 for(let i=0; i<5; i++){
     let firstLi = document.createElement('li');
     let firstLiText = document.createTextNode('test'+i);
-    let secondOl = document.createElement('ol');    
+    let secondOl = document.createElement('ol');
+    let rightAns = document.createTextNode('答對了');
+    let wrongAns = document.createTextNode('答錯了');
     firstLi.appendChild(firstLiText);
     for(let j=5; j<9; j++){
         let secondLi = document.createElement('li');
@@ -12,14 +14,15 @@ for(let i=0; i<5; i++){
         let label = document.createElement('label');
         let br = document.createElement('br');
         radioButton.type = 'radio';
-        radioButton.name = 'radio' + '_' + i;        
+        radioButton.name = 'radio' + '_' + i;
         secondLi.appendChild(secondLiText);
         label.appendChild(radioButton);
         label.appendChild(secondLi);
         label.appendChild(br);
-        secondOl.appendChild(label);
-        firstLi.appendChild(secondOl);
+        secondOl.appendChild(label);        
+        firstLi.appendChild(secondOl);        
     }
+    firstLi.childNodes[0].nodeValue = "add Text " + firstLi.childNodes[0].nodeValue;
     firstOl.appendChild(firstLi);
 }
 fragment.appendChild(firstOl);
