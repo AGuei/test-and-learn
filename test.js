488594ed606cd74aea919ec19e438f926dc39892
貴 <AGuei@users.noreply.github.com>
2017-10-14 14:46:16 +0800

    巢狀清單

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 let fragment = document.createDocumentFragment();
 let firstUl = document.createElement('ol');
-let secondUl = document.createElement('ol');
 for(let i = 0; i < 5; i++){
     let firstLi = document.createElement('li');
-    let secondLi = document.createElement('li');    
     let firstLiText = document.createTextNode('test'+i);
-    let secondLiText = document.createTextNode('test'+i);    
-    firstLi.appendChild(firstLiText);    
-    secondLi.appendChild(secondLiText);
-    firstUl.appendChild(firstLi);
-    secondUl.appendChild(secondLi);
+    let secondUl = document.createElement('ol');
+    firstLi.appendChild(firstLiText);
+    for(let j=5; j<9; j++){
+        let secondLi = document.createElement('li');    
+        let secondLiText = document.createTextNode('test'+j);
+        secondLi.appendChild(secondLiText);
+        secondUl.appendChild(secondLi);
+        firstLi.appendChild(secondUl);
+    }
+    firstUl.appendChild(firstLi);    
 }
 fragment.appendChild(firstUl);
-fragment.appendChild(secondUl);
 document.body.appendChild(fragment);
